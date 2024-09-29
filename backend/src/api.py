33104bc8b7dbef01c0657d79a53a5d627aa401d2
BeaverNotACat <beavernotacat@gmail.com>
2024-09-29 16:49:44 +1000

    Add predictions

--- backend/src/api.py
+++ backend/src/api.py
@@ -3,18 +3,18 @@ from uuid import uuid4
 from fastapi import APIRouter
 
 from src.schemas import RecommendInput, RecommendOutput
-from src.service import model
+from src.service import predict, fit
+from src.database import dataset
 
 
 router = APIRouter()
 
 
 @router.post("/predict", response_model=RecommendOutput)
-async def rocmmend_videos(reactions: RecommendInput):
-    return [ 
-        {
-            "videoId": uuid4(),
-            "name": "Соус кальве",
-            "description": "Ох уж этот мистер джонсон. Опят всё перепутал" 
-        } for _ in range(10)
-    ]
+async def recommend_videos(reactions: RecommendInput):
+    return predict(RecommendOutput)
+
+
+@router.post("/fit")
+async def rocmmend_videos():
+    return fit(dataset)
--- backend/src/service.py
+++ backend/src/service.py
@@ -1,12 +1,31 @@
-from src.database import dataset
-from src.schemas import RecommendInput 
+from src.database import dataset, videos, interactions
+from src.schemas import RecommendInput, RecommendOutput
 
 from lightfm import LightFM
 from rectools.models import LightFMWrapperModel
 
 model=LightFM(no_components=30,loss="warp")
 wrapped_model=LightFMWrapperModel(model)
-wrapped_model=wrapped_model.fit(dataset)
+
+
+def fit(dataset):
+    return wrapped_model.fit(dataset)
+    
 
 def predict(data: RecommendInput):
-    model.predict()
+    temsToRec = wrapped_model.recommend([data.userId], dataset, 10,True)
+    scores = []
+    vIds=[]
+    i=0
+
+    res = []
+    for c, item in temsToRec.iterrows():
+        print(item["item_id"])
+        video=list(videos[videos["video_id"].values==item["item_id"]].iterrows())[0]
+        # print(video)
+        print(video[1].title)
+        print(video[1].description)
+        
+        i+=1
+
+        res.append({"videoId": item["item_id"], "name": video[1].title, "description": video[1].description})
